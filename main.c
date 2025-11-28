cb88398a8395584f7da5a3deb1b0a44b49913aef
Jeevesh Kumar <rajatjeevesh513@gmail.com>
2025-11-28 10:44:48 +0000

    created structures

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define MAXR 50
+#define MAXS 50
+
+struct roominfo{
+    int rno;
+    int count;
+    int money;
+};
+
+struct studinfo{
+    int sid;
+    char sname[50];
+    int rno;
+};
+
+
+
+int main(){
+    return 0;
+}
